12b2be5118b845d4487b4b3d406749b6082fb53d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-10 16:02:29 -0400

    style: p6ify

--- init.zsh
+++ init.zsh
@@ -1,3 +1,4 @@
+# shellcheck shell=bash
 ######################################################################
 #<
 #
@@ -21,6 +22,8 @@ p6df::modules::datadog::deps() {
 p6df::modules::datadog::langs() {
 
   pip install datadog # dog cli
+
+  p6_return_void
 }
 
 ######################################################################
@@ -34,15 +37,6 @@ p6df::modules::datadog::langs() {
 p6df::modules::datadog::clones() {
 
   p6_github_login_clone DataDog "$P6_DFZ_SRC_DIR"
-}
 
-######################################################################
-#<
-#
-# Function: p6df::modules::datadog::init()
-#
-#>
-######################################################################
-p6df::modules::datadog::init() {
-  true
+  p6_return_void
 }
