5e2e8dd6a0608484eba4bf91435af640f8b0a353
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-03 09:11:34 -0500

    chore: moves P6_DFZ_SRC_DIR -> P6_DFZ_SRC_FOCUSED_DIR

--- init.zsh
+++ init.zsh
@@ -37,12 +37,12 @@ p6df::modules::datadog::langs() {
 #
 # Function: p6df::modules::datadog::clones()
 #
-#  Environment:	 P6_DFZ_SRC_DIR
+#  Environment:	 P6_DFZ_SRC_FOCUSED_DIR
 #>
 ######################################################################
 p6df::modules::datadog::clones() {
 
-  p6_github_login_clone DataDog "$P6_DFZ_SRC_DIR"
+  p6_github_login_clone DataDog "$P6_DFZ_SRC_FOCUSED_DIR"
 
   p6_return_void
 }
